24b2ee79b9c4e8858bb79f2a048183c923e7696e
sukoshi <duongminhkhoa@Duongs-MacBook-Pro.local>
2015-01-16 00:24:24 +0900

    add origin anim, test font

---
+++ Assets/Script/Game/UI/GamePlay/BlockOriginAnim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockOriginAnim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UIWidget widget = GetComponent<UIWidget> ();
+		widget.depth = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnAnimComplete()
+	{
+		Destroy (gameObject);
+	}
+}
